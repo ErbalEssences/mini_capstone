174880161feb8ee3f76bf03c616d2e4fbd2b4953
ErbalEssences <LieseErb.com>
2019-05-02 09:35:05 -0500

    added migrate folder and info

---
+++ db/migrate/20190502142631_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :price
+      t.string :image_url
+      t.string :description
+      t.string :material
+      t.string :type
+      t.string :style
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+item = ModelName.new({attribute1: “some value”, attribute2: “some value”})
+item.save
+
+
+name:string price:string image_url:string description:string material:string type:"earrings" style:"short"
